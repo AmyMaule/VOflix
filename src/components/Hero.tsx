370db9b9660cbf17e41f873f436420b82ab5c455
Amy <amaule65@gmail.com>
2024-06-07 18:42:27 +0200

    Update animations for showings and hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <div className="hero-text-container">
-        <h1 className="hero-title">Hello this is a title</h1>
+        <h1 className="hero-title animate">Hello this is a title</h1>
         <div className="zigzag"></div>
         <p className="hero-subtitle">And this is a text</p>
       </div>
