0366c02137927457b78fc7980eae6814296b06bb
Amy <amaule65@gmail.com>
2024-07-06 13:38:00 +0200

    Update hero text and background images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,13 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <div className="hero-text-container">
-        <h1 className="hero-title animate">Hello this is a title</h1>
-        <div className="zigzag"></div>
-        <p className="hero-subtitle">And this is a text</p>
+        <h1 className="hero-title animate">
+          With <span className="hero-title-logo">V.O.flix</span>, English cinema is just around the corner
+        </h1>
+        <div className="line-separator" />
+        <p className="hero-subtitle">
+          V.O.flix is your go-to site for finding English-language films showing at your local cinema with up-to-date showtimes so you never miss a film!
+        </p>
       </div>
     </div>
   )
