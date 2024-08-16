90dd2623f8c5bab020a02f25e21ea60409e6bf5e
Amy <amaule65@gmail.com>
2024-08-16 16:41:51 +0200

    Add attribution for TMDB

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,9 @@ const App = () => {
     <div className="app-container">
       <Hero />
       <FilmsSection />
+      <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+        <img src="TMDB-logo.svg" className="tmdb-logo" />
+      </a>
     </div>
   )
 }
