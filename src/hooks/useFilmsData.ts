0bbedcc0557e5b7e4c2c9087cc8fa63962989081
Amy <amaule65@gmail.com>
2026-06-03 15:27:30 +0200

    Add pageshow event for refetch

--- src/hooks/useFilmsData.ts
+++ src/hooks/useFilmsData.ts
@@ -18,53 +18,49 @@ export const useFilmsData = () => {
   const [showings, setShowings] = useState<RawShowingType[]>([]);
   const [cinemas, setCinemas] = useState<Record<string, CinemaType>>({});
 
-const loadData = useCallback(async () => {
-  try {
-    const [filmsResult, showingsResult, cinemasResult] = await Promise.all([
-      filmsApi.getFilms(),
-      filmsApi.getShowings(),
-      filmsApi.getCinemas(),
-    ]);
+  const loadData = useCallback(async () => {
+    try {
+      const [filmsResult, showingsResult, cinemasResult] = await Promise.all([
+        filmsApi.getFilms(),
+        filmsApi.getShowings(),
+        filmsApi.getCinemas(),
+      ]);
 
-    console.log("Fetching new data for Lavelanet, Foix and Perpignan...")
-    setAllFilmData(filmsResult);
-    setShowings(showingsResult);
-    setCinemas(cinemasResult);
-    setLastFetchedTime(Date.now());
-    const testShowings: RawShowingType[] = [];
-    const testCinemas = ["Méga Castillet,Perpignan", "Le Casino,Lavelanet", "L'Estive,Foix", "Castillet,Perpignan"]
-    showingsResult.forEach(showing => {
-      if (testCinemas.includes(showing.cinema)) {
-        testShowings.push(showing);
-      }
-    })
-    console.log(testShowings)
-  } catch (err) {
-    console.error("Failed to load data", err);
-  }
-}, []);
+      console.log("Fetching films...")
+      setAllFilmData(filmsResult);
+      setShowings(showingsResult);
+      setCinemas(cinemasResult);
+      setLastFetchedTime(Date.now());
+    } catch (err) {
+      console.error("Failed to load data", err);
+    }
+  }, []);
 
   // Initial load
   useEffect(() => {
     loadData();
   }, [loadData]);
 
-  // When tab changes focus
   useEffect(() => {
-    // If last fetch was > 6 hours ago, refetch
+    // If last fetch was > 3 hours ago, refetch
     const checkForRefetch = () => {
       if (document.visibilityState !== "visible") return;
       const lastFetchedTime = getLastFetchedTime();
       const now = Date.now();
       const threeHours = 3 * 60 * 60 * 1000;
-      console.log("Checking for refetch", now - lastFetchedTime)
+      console.log("Checking for refetch", !lastFetchedTime, now - lastFetchedTime > threeHours)
       if (!lastFetchedTime || now - lastFetchedTime > threeHours) {
         loadData();
       }
     };
 
+    // Check when the browser restores the page as well as when the user switches tabs
     document.addEventListener("visibilitychange", checkForRefetch);
-    return () => document.removeEventListener("visibilitychange", checkForRefetch);
+    window.addEventListener("pageshow", checkForRefetch);
+    return () => {
+      document.removeEventListener("visibilitychange", checkForRefetch);
+      window.removeEventListener("pageshow", checkForRefetch);
+    }
   }, [loadData]);
 
   return {
